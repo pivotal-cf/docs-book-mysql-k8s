8b32eec242f0035eb628b9cfa13d48b4ff116e63
Chris Wong <cwong@pivotal.io>
2020-11-10 18:29:33 -0500

    Fix things that weren't right

--- redirects.rb
+++ redirects.rb
@@ -2,10 +2,3 @@
 r301 %r{.*}, 'https://docs.pivotal.io$&', :if => Proc.new { |rack_env|
   rack_env['SERVER_NAME'] == 'docs.pivotal.io' && rack_env['HTTP_X_FORWARDED_PROTO'] == 'http'
 }
-
-r301 %r{/p-mysql/([\d-]+)/minor-overview.html}, '/p-mysql/$1/index.html'
-r301 %r{/p-mysql/([\d-]+)/modify-apps-tls.html}, '/p-mysql/$1/using-tls.html'
-r301 %r{/p-mysql/([\d-]+)/installing.html}, '/p-mysql/$1/install-config.html'
-r301 %r{/p-mysql/([\d-]+)/backup-and-restore.html}, '/p-mysql/$1/backup-restore.html'
-r301 %r{/p-mysql/([\d-]+)/backup.html}, '/p-mysql/$1/automated-backup.html'
-r301 %r{/p-mysql/([\d-]+)/maintain-leader-follower.html}, '/p-mysql/$1/about-leader-follower.html'
