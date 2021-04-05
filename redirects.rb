3a517db4047ab6b795b913c680f2b84d4e696f66
Ben Klein <bklein@pivotal.io>
2021-04-05 14:48:24 -0500

    Redirect for 'Connecting Apps' topic
    The 'Connecting' topic filename has been renamed to not use the term
    'bind'.
    
    [#177568993]

--- redirects.rb
+++ redirects.rb
@@ -2,3 +2,5 @@
 r301 %r{.*}, 'https://docs.pivotal.io$&', :if => Proc.new { |rack_env|
   rack_env['SERVER_NAME'] == 'docs.pivotal.io' && rack_env['HTTP_X_FORWARDED_PROTO'] == 'http'
 }
+
+r301 '/tanzu-mysql-kubernetes/1-n/bind-app.html', '/tanzu-mysql-kubernetes/1-n/connecting-apps.html'
