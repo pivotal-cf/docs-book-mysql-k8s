8410097fb3bb4e40af6c88930a21d1cb7d277954
Lena Hunter <lhunter@pivotal.io>
2021-07-26 06:49:41 -0700

    some changes for 1.1

--- redirects.rb
+++ redirects.rb
@@ -3,4 +3,9 @@ r301 %r{.*}, 'https://docs.pivotal.io$&', :if => Proc.new { |rack_env|
   rack_env['SERVER_NAME'] == 'docs.pivotal.io' && rack_env['HTTP_X_FORWARDED_PROTO'] == 'http'
 }
 
+r301 %r{/latest/(.*)}, '/tanzu-mysql-kubernetes/1-1/$1'
+r301 '/', '/tanzu-mysql-kubernetes/1-1/index.html'
+r301 '/index.html', '/tanzu-mysql-kubernetes/1-1/index.html'
+r301 '/1-1', '/tanzu-mysql-kubernetes/1-1/index.html'
+r301 '/1-0', '/tanzu-mysql-kubernetes/1-1/index.html'
 r301 '/tanzu-mysql-kubernetes/1-n/bind-app.html', '/tanzu-mysql-kubernetes/1-n/connecting-apps.html'
