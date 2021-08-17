ede58d38b151e93c1f97781363349ca7f1ddd613
David Yozie <dyozie@pivotal.io>
2021-08-17 14:52:02 -0700

    Fixing redirects for app route

--- redirects.rb
+++ redirects.rb
@@ -2,15 +2,15 @@
 r301 %r{.*}, 'https://docs.pivotal.io$&', :if => Proc.new { |rack_env|
   rack_env['SERVER_NAME'] == 'docs.pivotal.io' && rack_env['HTTP_X_FORWARDED_PROTO'] == 'http'
 }
-r301 %r{/latest/(.*)}, '/1-1/$1'
-r301 '/',           '/tanzu-mysql-kubernetes/1-1/index.html'
-r301 '/index.html', '/tanzu-mysql-kubernetes/1-1/index.html'
+r301 %r{/tanzu-mysql-kubernetes/latest/(.*)}, '/tanzu-mysql-kubernetes/1-1/$1'
+r301 '/tanzu-mysql-kubernetes/',           '/tanzu-mysql-kubernetes/1-1/index.html'
+r301 '/tanzu-mysql-kubernetes/index.html', '/tanzu-mysql-kubernetes/1-1/index.html'
 r301 '/tanzu-mysql-kubernetes/1-0/monitoring.html', '/tanzu-mysql-kubernetes/1-0/index.html'
 r301 '/tanzu-mysql-kubernetes/1-0/upgrade-instance.html', '/tanzu-mysql-kubernetes/1-0/index.html'
 r301 '/tanzu-mysql-kubernetes/1-0/connecting-tas.html', '/tanzu-mysql-kubernetes/1-0/index.html'
-r301 '/1-1', '/tanzu-mysql-kubernetes/1-1/index.html'
-r301 '/1-1/', '/tanzu-mysql-kubernetes/1-1/index.html'
-r301 '/1-0', '/tanzu-mysql-kubernetes/1-0/index.html'
-r301 '/1-0/', '/tanzu-mysql-kubernetes/1-0/index.html'
+r301 '/tanzu-mysql-kubernetes/1-1', '/tanzu-mysql-kubernetes/1-1/index.html'
+r301 '/tanzu-mysql-kubernetes/1-1/', '/tanzu-mysql-kubernetes/1-1/index.html'
+r301 '/tanzu-mysql-kubernetes/1-0', '/tanzu-mysql-kubernetes/1-0/index.html'
+r301 '/tanzu-mysql-kubernetes/1-0/', '/tanzu-mysql-kubernetes/1-0/index.html'
 
 
