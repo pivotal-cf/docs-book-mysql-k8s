426f6225850481d0245e3a4699ea4be8c64cc667
Lena Hunter <lhunter@pivotal.io>
2022-01-25 15:27:29 -0800

    fixed typo

--- redirects.rb
+++ redirects.rb
@@ -3,7 +3,7 @@ r301 %r{.*}, 'https://docs.pivotal.io$&', :if => Proc.new { |rack_env|
   rack_env['SERVER_NAME'] == 'docs.pivotal.io' && rack_env['HTTP_X_FORWARDED_PROTO'] == 'http'
 }
 
-r301 %r{(.*)}, 'https://docs.vmware.com/en/VMware-Tanzu-SQL-with-Postgres-for-Kubernetes/index.html'
+r301 %r{(.*)}, 'https://docs.vmware.com/en/VMware-Tanzu-SQL-with-MySQL-for-Kubernetes/index.html'
 
 
 
