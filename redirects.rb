74b90a7465df285b7383a87369370173e0474ee4
Lena Hunter <lhunter@pivotal.io>
2022-02-10 13:33:49 -0800

    fixes redirects

--- redirects.rb
+++ redirects.rb
@@ -3,8 +3,7 @@ r301 %r{.*}, 'https://docs.pivotal.io$&', :if => Proc.new { |rack_env|
   rack_env['SERVER_NAME'] == 'docs.pivotal.io' && rack_env['HTTP_X_FORWARDED_PROTO'] == 'http'
 }
 
-r301 %r{(.*)}, 'https://docs.vmware.com/en/VMware-Tanzu-SQL-with-MySQL-for-Kubernetes/index.html'
-
+r301 %r{(/1-2/*)}, 'https://docs.vmware.com/en/VMware-Tanzu-SQL-with-MySQL-for-Kubernetes/1.2/tanzu-mysql-k8s/GUID-index.html'
 
 
 
